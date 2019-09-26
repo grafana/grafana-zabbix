bfca76650db57e6aad083cf87171694992b60528
Alexander Zobnin <alexanderzobnin@gmail.com>
2019-09-26 09:32:22 +0300

    backend: proxy Zabbix API requests WIP

--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.12
 
 require (
 	github.com/bitly/go-simplejson v0.5.0
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/grafana/grafana_plugin_model v0.0.0-20180518082423-84176c64269d
 	github.com/hashicorp/go-hclog v0.9.2
 	github.com/hashicorp/go-plugin v1.0.1
+	github.com/kr/pretty v0.1.0 // indirect
 	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d
 )
--- pkg/datasource.go
+++ pkg/datasource.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"time"
 
 	simplejson "github.com/bitly/go-simplejson"
 	"github.com/grafana/grafana_plugin_model/go/datasource"
 	hclog "github.com/hashicorp/go-hclog"
 	plugin "github.com/hashicorp/go-plugin"
 	"golang.org/x/net/context"
+	"golang.org/x/net/context/ctxhttp"
 )
 
 type ZabbixDatasource struct {
@@ -63,5 +74,95 @@ func (ds *ZabbixDatasource) ZabbixAPIQuery(ctx context.Context, tsdbReq *datasou
 		jsonQueries = append(jsonQueries, json)
 	}
 
+	if len(jsonQueries) == 0 {
+		return nil, errors.New("At least one query should be provided")
+	}
+
+	jsonQuery := jsonQueries[0].Get("target")
+
+	// TODO: inject auth token (obtain from 'user.login' first)
+	reqBodyJson, err := json.Marshal(map[string]interface{}{
+		"jsonrpc": "2.0",
+		"id":      2,
+		"method":  jsonQuery.Get("method").MustString(),
+		"params":  jsonQuery.Get("params"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	dsInfo := tsdbReq.GetDatasource()
+	zabbixUrlStr := dsInfo.GetUrl()
+	zabbixUrl, err := url.Parse(zabbixUrlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	jsonDataStr := dsInfo.GetJsonData()
+	jsonData, err := simplejson.NewJson([]byte(jsonDataStr))
+	if err != nil {
+		return nil, err
+	}
+
+	zabbixLogin := jsonData.Get("username").MustString()
+	// zabbixPassword := jsonData.Get("password").MustString()
+	ds.logger.Debug("ZabbixAPIQuery", "url", zabbixUrl, "user", zabbixLogin)
+
+	var body io.Reader
+	body = bytes.NewReader(reqBodyJson)
+	rc, ok := body.(io.ReadCloser)
+	if !ok && body != nil {
+		rc = ioutil.NopCloser(body)
+	}
+
+	req := &http.Request{
+		Method: "POST",
+		URL:    zabbixUrl,
+		Header: map[string][]string{
+			"Content-Type": {"application/json"},
+		},
+		Body: rc,
+	}
+
+	response, err := ds.MakeHttpRequest(ctx, req)
+	ds.logger.Debug("ZabbixAPIQuery", "response", string(response))
+
 	return nil, errors.New("ZabbixAPIQuery is not implemented yet")
+}
+
+func (ds *ZabbixDatasource) MakeHttpRequest(ctx context.Context, req *http.Request) ([]byte, error) {
+	res, err := ctxhttp.Do(ctx, httpClient, req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("invalid status code. status: %v", res.Status)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+var httpClient = &http.Client{
+	Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{
+			Renegotiation: tls.RenegotiateFreelyAsClient,
+		},
+		Proxy: http.ProxyFromEnvironment,
+		Dial: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).Dial,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+	},
+	Timeout: time.Duration(time.Second * 30),
 }
