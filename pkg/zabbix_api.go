4f5f5ecdc2441335f776b988f3ef3af51adb021c
BC064890 <BC064890@cerner.net>
2019-10-04 14:40:00 -0500

    Added case if the err is nil

--- pkg/zabbix_api.go
+++ pkg/zabbix_api.go
@@ -123,7 +123,7 @@ func (ds *ZabbixDatasource) ZabbixRequest(ctx context.Context, dsInfo *datasourc
 		}
 		zabbixAuth = auth
 		result, err = ds.zabbixAPIRequest(ctx, zabbixUrl, method, params, zabbixAuth)
-		if err != nil && !isNotAuthorized(err.Error()) {
+		if err == nil || (err != nil && !isNotAuthorized(err.Error())) {
 			break
 		}
 	}
