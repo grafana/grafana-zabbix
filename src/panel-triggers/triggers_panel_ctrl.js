0d3603bde9ac8c8980ae0a4fedc90f8f3a9a62a6
Arnis Civciss <arnis.civciss@lattelecom.lv>
2020-01-13 15:12:06 +0200

    fixed panel trigger severity if it is changed in event

--- src/panel-triggers/triggers_panel_ctrl.js
+++ src/panel-triggers/triggers_panel_ctrl.js
@@ -398,7 +398,12 @@ export class TriggerPanelCtrl extends PanelCtrl {
 
     // Filter triggers by severity
     triggerList = _.filter(triggerList, trigger => {
-      return this.panel.triggerSeverity[trigger.priority].show;
+      //return this.panel.triggerSeverity[trigger.priority].show;
+      if (trigger.lastEvent.severity) {
+        return this.panel.triggerSeverity[trigger.lastEvent.severity].show;
+      } else {
+        return this.panel.triggerSeverity[trigger.priority].show;
+      }
     });
 
     return triggerList;
