04715e94c888a547dc9fcda5ba903969b7ffa513
Alexander Zobnin <alexanderzobnin@gmail.com>
2020-05-08 17:13:54 +0300

    Chore: remove unused files

--- src/panel-triggers/module.js
+++ src/panel-triggers/module.js
@@ -11,9 +11,8 @@
  * Licensed under the Apache License, Version 2.0
  */
 
-import {TriggerPanelCtrl} from './triggers_panel_ctrl';
-import {loadPluginCss} from 'grafana/app/plugins/sdk';
-import './datasource-selector.directive';
+import { TriggerPanelCtrl } from './triggers_panel_ctrl';
+import { loadPluginCss } from 'grafana/app/plugins/sdk';
 
 loadPluginCss({
   dark: 'plugins/alexanderzobnin-zabbix-app/css/grafana-zabbix.dark.css',
