de73f5659ea57bd8623f49b385fe3e1c1720e4c2
Alexander Zobnin <alexanderzobnin@gmail.com>
2020-12-15 12:17:08 +0300

    Functions for aligning timeseries on the backend

--- pkg/datasource/zabbix.go
+++ pkg/datasource/zabbix.go
@@ -374,7 +374,8 @@ func (ds *ZabbixDatasourceInstance) queryNumericDataForItems(ctx context.Context
 		return nil, err
 	}
 
-	return convertHistory(history, items), nil
+	frame := convertHistory(history, items)
+	return frame, nil
 }
 
 func (ds *ZabbixDatasourceInstance) getTrendValueType(query *QueryModel) string {
@@ -470,46 +471,6 @@ func (ds *ZabbixDatasourceInstance) isUseTrend(timeRange backend.TimeRange) bool
 		return true
 	}
 	return false
-}
-
-func convertHistory(history History, items Items) *data.Frame {
-	timeFileld := data.NewFieldFromFieldType(data.FieldTypeTime, 0)
-	timeFileld.Name = "time"
-	frame := data.NewFrame("History", timeFileld)
-
-	for _, item := range items {
-		field := data.NewFieldFromFieldType(data.FieldTypeNullableFloat64, 0)
-		if len(item.Hosts) > 0 {
-			field.Name = fmt.Sprintf("%s: %s", item.Hosts[0].Name, item.ExpandItem())
-		} else {
-			field.Name = item.ExpandItem()
-		}
-		frame.Fields = append(frame.Fields, field)
-	}
-
-	for _, point := range history {
-		for columnIndex, field := range frame.Fields {
-			if columnIndex == 0 {
-				ts := time.Unix(point.Clock, point.NS)
-				field.Append(ts)
-			} else {
-				item := items[columnIndex-1]
-				if point.ItemID == item.ID {
-					value := point.Value
-					field.Append(&value)
-				} else {
-					field.Append(nil)
-				}
-			}
-		}
-	}
-
-	// TODO: convert to wide format
-	wideFrame, err := data.LongToWide(frame, &data.FillMissing{Mode: data.FillModeNull})
-	if err == nil {
-		return wideFrame
-	}
-	return frame
 }
 
 func parseFilter(filter string) (*regexp.Regexp, error) {
