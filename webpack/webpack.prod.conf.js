b4e0a49e0790670fa32deb9c8bbdcb54d3605c28
Alexander Zobnin <alexanderzobnin@gmail.com>
2021-08-10 16:53:33 +0300

    Fix production build

--- webpack/webpack.prod.conf.js
+++ webpack/webpack.prod.conf.js
@@ -1,9 +1,7 @@
 const baseWebpackConfig = require('./webpack.base.conf');
-const NgAnnotatePlugin = require('ng-annotate-webpack-plugin');
 
-var conf = baseWebpackConfig;
+const conf = baseWebpackConfig;
 conf.mode = 'production';
 conf.devtool = 'source-map';
-conf.plugins.push(new NgAnnotatePlugin());
 
 module.exports = baseWebpackConfig;
