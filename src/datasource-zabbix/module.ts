be2f214b1d2f1434b1291e9ea7e9adf306c5ef4d
Alexander Zobnin <alexanderzobnin@gmail.com>
2022-11-08 16:37:46 +0300

    Remove angular query editor

--- src/datasource-zabbix/module.ts
+++ src/datasource-zabbix/module.ts
@@ -1,20 +1,15 @@
 import { DataSourcePlugin } from '@grafana/data';
 import { loadPluginCss } from '@grafana/runtime';
 import { ZabbixDatasource } from './datasource';
-import { ZabbixQueryController } from './query.controller';
 import { QueryEditor } from './components/QueryEditor';
 import { ZabbixVariableQueryEditor } from './components/VariableQueryEditor';
 import { ConfigEditor } from './components/ConfigEditor';
-import './add-metric-function.directive';
-import './metric-function-editor.directive';
 import '../sass/grafana-zabbix.dark.scss';
 import '../sass/grafana-zabbix.light.scss';
 
 class ZabbixAnnotationsQueryController {
   static templateUrl = 'datasource-zabbix/partials/annotations.editor.html';
 }
-
-ZabbixQueryController.templateUrl = 'datasource-zabbix/partials/query.editor.html';
 
 loadPluginCss({
   dark: 'plugins/alexanderzobnin-zabbix-app/css/grafana-zabbix.dark.css',
@@ -23,7 +18,6 @@ loadPluginCss({
 
 export const plugin = new DataSourcePlugin(ZabbixDatasource)
   .setConfigEditor(ConfigEditor)
-  // .setQueryCtrl(ZabbixQueryController)
   .setQueryEditor(QueryEditor)
   .setAnnotationQueryCtrl(ZabbixAnnotationsQueryController)
   .setVariableQueryEditor(ZabbixVariableQueryEditor);
