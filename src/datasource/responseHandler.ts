583271319390d0fadd897df7b497c84a504b0715
Konstantin Kornienko <konstantin.kornienko@incountry.com>
2023-02-10 07:49:01 +0000

    v4.2.12

--- src/datasource/responseHandler.ts
+++ src/datasource/responseHandler.ts
@@ -471,10 +471,7 @@ export function handleSLIResponse(response: any, itservices: any[], target: Zabb
   const timestamps = [];
   for (let i = 0; i < response?.periods?.length; i++) {
     const period = response.periods[i];
-    if (i === 0) {
-      timestamps.push(period.period_from * 1000);
-    }
-    timestamps.push(period.period_to * 1000);
+    timestamps.push(period.period_from * 1000);
   }
 
   const timeFiled: Field = {
@@ -497,9 +494,6 @@ export function handleSLIResponse(response: any, itservices: any[], target: Zabb
       const value = sli[slaProperty];
       if (!values[j]) {
         values[j] = [];
-      }
-      if (i === 0) {
-        values[j].push(value);
       }
       values[j].push(value);
     }
