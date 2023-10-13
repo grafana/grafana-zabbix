43f2c6653bda5a0dcd9b824a11839d2531202906
Zoltán Bedi <zoltan.bedi@gmail.com>
2023-10-13 15:20:31 +0200

    Chore: Fix prettier issues in test files (#1710)
    * Chore: Fix prettier issues in test files
    
    * Escape w and d properly

--- src/datasource/specs/utils.spec.ts
+++ src/datasource/specs/utils.spec.ts
@@ -2,29 +2,27 @@ import _ from 'lodash';
 import * as utils from '../utils';
 
 describe('Utils', () => {
-
   describe('expandItemName()', () => {
-
     it('should properly expand unquoted params', (done) => {
       let test_cases = [
         {
           name: `CPU $2 time`,
           key: `system.cpu.util[,user,avg1]`,
-          expected: "CPU user time"
+          expected: 'CPU user time',
         },
         {
           name: `CPU $2 time - $3`,
           key: `system.cpu.util[,system,avg1]`,
-          expected: "CPU system time - avg1"
+          expected: 'CPU system time - avg1',
         },
         {
           name: `CPU - $1 - $2 - $3`,
           key: `system.cpu.util[,system,avg1]`,
-          expected: "CPU -  - system - avg1"
-        }
+          expected: 'CPU -  - system - avg1',
+        },
       ];
 
-      _.each(test_cases, test_case => {
+      _.each(test_cases, (test_case) => {
         let expandedName = utils.expandItemName(test_case.name, test_case.key);
         expect(expandedName).toBe(test_case.expected);
       });
@@ -36,26 +34,26 @@ describe('Utils', () => {
         {
           name: `CPU $2 time`,
           key: `system.cpu.util["type=user,value=avg",user]`,
-          expected: "CPU user time"
+          expected: 'CPU user time',
         },
         {
           name: `CPU $1 time`,
           key: `system.cpu.util["type=user,value=avg","user"]`,
-          expected: "CPU type=user,value=avg time"
+          expected: 'CPU type=user,value=avg time',
         },
         {
           name: `CPU $1 time $3`,
           key: `system.cpu.util["type=user,value=avg",,"user"]`,
-          expected: "CPU type=user,value=avg time user"
+          expected: 'CPU type=user,value=avg time user',
         },
         {
           name: `CPU $1 $2 $3`,
           key: `system.cpu.util["type=user,value=avg",time,"user"]`,
-          expected: "CPU type=user,value=avg time user"
-        }
+          expected: 'CPU type=user,value=avg time user',
+        },
       ];
 
-      _.each(test_cases, test_case => {
+      _.each(test_cases, (test_case) => {
         let expandedName = utils.expandItemName(test_case.name, test_case.key);
         expect(expandedName).toBe(test_case.expected);
       });
@@ -67,21 +65,21 @@ describe('Utils', () => {
         {
           name: `CPU $2 - $3 time`,
           key: `system.cpu.util[,[user,system],avg1]`,
-          expected: "CPU user,system - avg1 time"
+          expected: 'CPU user,system - avg1 time',
         },
         {
           name: `CPU $2 - $3 time`,
           key: `system.cpu.util[,["user,system",iowait],avg1]`,
-          expected: `CPU "user,system",iowait - avg1 time`
+          expected: `CPU "user,system",iowait - avg1 time`,
         },
         {
           name: `CPU - $2 - $3 - $4`,
           key: `system.cpu.util[,[],["user,system",iowait],avg1]`,
-          expected: `CPU -  - "user,system",iowait - avg1`
-        }
+          expected: `CPU -  - "user,system",iowait - avg1`,
+        },
       ];
 
-      _.each(test_cases, test_case => {
+      _.each(test_cases, (test_case) => {
         let expandedName = utils.expandItemName(test_case.name, test_case.key);
         expect(expandedName).toBe(test_case.expected);
       });
@@ -90,21 +88,20 @@ describe('Utils', () => {
   });
 
   describe('splitTemplateQuery()', () => {
-
     // Backward compatibility
     it('should properly split query in old format', (done) => {
       let test_cases = [
         {
-          query: `/alu/./tw-(nyc|que|brx|dwt|brk)-sta_(\w|\d)*-alu-[0-9{2}/`,
-          expected: ['/alu/', '/tw-(nyc|que|brx|dwt|brk)-sta_(\w|\d)*-alu-[0-9{2}/']
+          query: `/alu/./tw-(nyc|que|brx|dwt|brk)-sta_(\\w|\\d)*-alu-[0-9{2}/`,
+          expected: ['/alu/', '/tw-(nyc|que|brx|dwt|brk)-sta_(\\w|\\d)*-alu-[0-9{2}/'],
         },
         {
           query: `a.b.c.d`,
-          expected: ['a', 'b', 'c', 'd']
-        }
+          expected: ['a', 'b', 'c', 'd'],
+        },
       ];
 
-      _.each(test_cases, test_case => {
+      _.each(test_cases, (test_case) => {
         let splitQuery = utils.splitTemplateQuery(test_case.query);
         expect(splitQuery).toEqual(test_case.expected);
       });
@@ -114,24 +111,24 @@ describe('Utils', () => {
     it('should properly split query', (done) => {
       let test_cases = [
         {
-          query: `{alu}{/tw-(nyc|que|brx|dwt|brk)-sta_(\w|\d)*-alu-[0-9]*/}`,
-          expected: ['alu', '/tw-(nyc|que|brx|dwt|brk)-sta_(\w|\d)*-alu-[0-9]*/']
+          query: `{alu}{/tw-(nyc|que|brx|dwt|brk)-sta_(\\w|\\d)*-alu-[0-9]*/}`,
+          expected: ['alu', '/tw-(nyc|que|brx|dwt|brk)-sta_(\\w|\\d)*-alu-[0-9]*/'],
         },
         {
-          query: `{alu}{/tw-(nyc|que|brx|dwt|brk)-sta_(\w|\d)*-alu-[0-9]{2}/}`,
-          expected: ['alu', '/tw-(nyc|que|brx|dwt|brk)-sta_(\w|\d)*-alu-[0-9]{2}/']
+          query: `{alu}{/tw-(nyc|que|brx|dwt|brk)-sta_(\\w|\\d)*-alu-[0-9]{2}/}`,
+          expected: ['alu', '/tw-(nyc|que|brx|dwt|brk)-sta_(\\w|\\d)*-alu-[0-9]{2}/'],
         },
         {
           query: `{a}{b}{c}{d}`,
-          expected: ['a', 'b', 'c', 'd']
+          expected: ['a', 'b', 'c', 'd'],
         },
         {
           query: `{a}{b.c.d}`,
-          expected: ['a', 'b.c.d']
-        }
+          expected: ['a', 'b.c.d'],
+        },
       ];
 
-      _.each(test_cases, test_case => {
+      _.each(test_cases, (test_case) => {
         let splitQuery = utils.splitTemplateQuery(test_case.query);
         expect(splitQuery).toEqual(test_case.expected);
       });
@@ -144,19 +141,31 @@ describe('Utils', () => {
       const test_cases = [
         {
           array: [],
-          depth: 1
+          depth: 1,
         },
         {
           array: [1, 2, 3],
-          depth: 1
+          depth: 1,
         },
         {
-          array: [[1, 2], [3, 4]],
-          depth: 2
+          array: [
+            [1, 2],
+            [3, 4],
+          ],
+          depth: 2,
         },
         {
-          array: [[[1, 2], [3, 4]], [[1, 2], [3, 4]]],
-          depth: 3
+          array: [
+            [
+              [1, 2],
+              [3, 4],
+            ],
+            [
+              [1, 2],
+              [3, 4],
+            ],
+          ],
+          depth: 3,
         },
       ];
 
