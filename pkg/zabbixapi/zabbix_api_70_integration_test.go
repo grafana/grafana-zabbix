50d18ec92f4a903b372bc737a3db48ba83c26dc3
Christos Diamantis <cdiamantis@adaptera.gr>
2025-08-04 10:27:36 +0300

    fix integration tests

--- pkg/zabbixapi/zabbix_api_70_integration_test.go
+++ pkg/zabbixapi/zabbix_api_70_integration_test.go
@@ -165,9 +165,10 @@ func TestIntegrationZabbixAPI70(t *testing.T) {
 		}
 
 		userId := zabbixUserResp.GetIndex(0).Get("userid").MustString()
+		userName := zabbixUserResp.GetIndex(0).Get("username").MustString()
 
 		// Generate or retrieve Zabbix API token for the user
-		token, err := api.GenerateUserAPIToken(context.Background(), userId, zabbixVersion)
+		token, err := api.GenerateUserAPIToken(context.Background(), userId, userName, zabbixVersion)
 		require.NoError(t, err)
 		assert.NotEmpty(t, token, "Generated token should not be empty")
 
