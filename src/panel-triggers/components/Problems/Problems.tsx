3a6cf556bb1d23fafe70de6d1cd66d02d3da0faa
ismail simsek <ismailsimsek09@gmail.com>
2025-12-03 15:23:20 +0100

    css updates

--- src/panel-triggers/components/Problems/Problems.tsx
+++ src/panel-triggers/components/Problems/Problems.tsx
@@ -230,20 +230,18 @@ export const ProblemList = (props: ProblemListProps) => {
   // console.log('Panel Options', panelOptions);
   return (
     <div className={cx('panel-problems', { [`font-size--${fontSize}`]: !!fontSize })} ref={rootRef}>
-      <div style={{ border: 'solid 1px red', marginBottom: '50px', padding: '5px' }}>
-        <ProblemsTable
-          rootWidth={rootRef?.current?.clientWidth || 0}
-          problems={problems}
-          panelOptions={panelOptions}
-          timeRange={timeRange}
-          getProblemEvents={getProblemEvents}
-          getProblemAlerts={getProblemAlerts}
-          getScripts={getScripts}
-          onProblemAck={handleProblemAck}
-          onExecuteScript={onExecuteScript}
-          onTagClick={handleTagClick}
-        />
-      </div>
+      <ProblemsTable
+        rootWidth={rootRef?.current?.clientWidth || 0}
+        problems={problems}
+        panelOptions={panelOptions}
+        timeRange={timeRange}
+        getProblemEvents={getProblemEvents}
+        getProblemAlerts={getProblemAlerts}
+        getScripts={getScripts}
+        onProblemAck={handleProblemAck}
+        onExecuteScript={onExecuteScript}
+        onTagClick={handleTagClick}
+      />
       <ReactTable
         data={problems}
         columns={columns}
