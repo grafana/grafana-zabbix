48557b1d0fbc859736d8d45b430129b3e3ae2397
Christos Diamantis <cdiamantis@adaptera.gr>
2026-01-09 12:47:13 +0200

    transferred reusable functions to utils.ts

---
+++ src/panel-triggers/components/Problems/utils.ts
@@ -0,0 +1,13 @@
+export const capitalizeFirstLetter = (str: string): string => 
+  str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+export const parseCustomTagColumns = (customTagColumns?: string): string[] => {
+    if (!customTagColumns) {
+        return [];
+    }
+
+    return customTagColumns
+        .split(',')
+        .map((tagName) => tagName.trim())
+        .filter(Boolean);
+};
